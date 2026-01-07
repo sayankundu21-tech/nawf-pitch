ae23723470427ea744d8cbedcf8fe0e89f0ef767
sayankundu21-tech <sayan.kundu21@iiad.edu.in>
2026-01-08 00:56:27 +0530

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import HorizontalVideoSection from './components/HorizontalVideoSection';
 import TextRevealSection from './components/TextRevealSection';
 import VennSection from './components/VennSection';
 import ArsenalSection from './components/ArsenalSection';
+import FloatingPillsSection from './components/FloatingPillsSection';
 
 const App: React.FC = () => {
   return (
@@ -26,6 +27,9 @@ const App: React.FC = () => {
 
       {/* Sixth Section: AI 360 Arsenal Network */}
       <ArsenalSection />
+
+      {/* Seventh Section: Floating Pills */}
+      <FloatingPillsSection />
 
       {/* Footer / Scroll hint */}
       <div className="fixed bottom-8 left-8 z-50 mix-blend-difference pointer-events-none">
